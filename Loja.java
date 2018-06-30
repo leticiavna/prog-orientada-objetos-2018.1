d7df5224f3e5ef2fce61b36364b3bc4218fb7db3
Letícia Vidal <leticiavna@id.uff.br>
2018-06-30 16:18:28 -0300

    melhora visualização do código

--- Loja.java
+++ Loja.java
@@ -3,131 +3,127 @@ import java.util.*;
 // Essa é a nossa main.
 
 public class Loja{
-    public static void main(String[] args) {
-				// Aqui declaramos o carrinho 
-				Carrinho Cart = new Carrinho();
-				
-				//Instanciamos uma variável Evento só por que fica chato ter que repetir td hora a mesma coisa
-				// e também pra legibilizar o código
-				Evento E;
+  public static void main(String[] args) {
+		// Aqui declaramos o carrinho 
+		Carrinho Cart = new Carrinho();
+		
+		//Instanciamos uma variável Evento só por que fica chato ter que repetir td hora a mesma coisa
+		// e também pra legibilizar o código
+		Evento E;
 
-        // Aqui declaramos os tipos de ingresso
-        TipoIngresso tipo1 = new TipoIngresso("Pista", 100.00);
-        TipoIngresso tipo2 = new TipoIngresso("Cadeira Superior", 200.00);
-        TipoIngresso tipo3 = new TipoIngresso("Cadeira Inferior", 150.00);
-        TipoIngresso tipo4 = new TipoIngresso("Arquibancada", 120.00);
-        TipoIngresso tipo5 = new TipoIngresso("Pista Galáctica", 400.00);
+		// Aqui declaramos os tipos de ingresso e seus valores
+    TipoIngresso tipo1 = new TipoIngresso("Pista", 100.00);
+    TipoIngresso tipo2 = new TipoIngresso("Cadeira Superior", 200.00);
+    TipoIngresso tipo3 = new TipoIngresso("Cadeira Inferior", 150.00);
+    TipoIngresso tipo4 = new TipoIngresso("Arquibancada", 120.00);
+    TipoIngresso tipo5 = new TipoIngresso("Pista Galáctica", 400.00);
 
-        // aqui instanciamos ingressos 
-        Ingresso i1 = new Ingresso(tipo1);
-        Ingresso i2 = new Ingresso(tipo2);
-        Ingresso i3 = new Ingresso(tipo3);
-        Ingresso i4 = new Ingresso(tipo4);
-        Ingresso i5 = new Ingresso(tipo5);
+    // aqui instanciamos ingressos 
+    Ingresso i1 = new Ingresso(tipo1);
+    Ingresso i2 = new Ingresso(tipo2);
+    Ingresso i3 = new Ingresso(tipo3);
+    Ingresso i4 = new Ingresso(tipo4);
+    Ingresso i5 = new Ingresso(tipo5);
+     
+    // aqui cria-se listas de ingressos para incluir nas instancias de evento
+    List<Ingresso> listIngresso1 = new ArrayList<>();
+    listIngresso1.add(i1);
+    listIngresso1.add(i2);
+    listIngresso1.add(i3);
+
+    List<Ingresso> listIngresso2 = new ArrayList<>();
+    listIngresso2.add(i1);
+    listIngresso2.add(i4);
+    listIngresso2.add(i5);
         
-        // aqui cria-se listas de ingressos para incluir nas instancias de evento
-        List<Ingresso> listIngresso1 = new ArrayList<>();
-        listIngresso1.add(i1);
-        listIngresso1.add(i2);
-        listIngresso1.add(i3);
 
-        List<Ingresso> listIngresso2 = new ArrayList<>();
-        listIngresso2.add(i1);
-        listIngresso2.add(i4);
-        listIngresso2.add(i5);
-        
-
-        // Aqui estamos criando as instâncias (objetos) que vão ser apresentadas no terminal
-        Evento evento1 = new Evento("Letícia e Mari dos Teclados", "Born Again", listIngresso1);
-        evento1.criaLocalData(1995, 1, 1, "Arena Ceres no Cinturão de Asteróide");
-        evento1.criaLocalData(2020, 9, 21, "Estádio de Alexandria");
-        evento1.criaLocalData(2006, 6, 6, "Broadway de Nova Nova York");
+    // Aqui estamos criando as instâncias (objetos) que vão ser apresentadas no terminal
+    Evento evento1 = new Evento("Letícia e Mari dos Teclados", "Born Again", listIngresso1);
+    evento1.criaLocalData(1995, 1, 1, "Arena Ceres no Cinturão de Asteróide");
+    evento1.criaLocalData(2020, 9, 21, "Estádio de Alexandria");
+    evento1.criaLocalData(2006, 6, 6, "Broadway de Nova Nova York");
 
         
-        Evento evento2 = new Evento("Let e Thales - Um Encontro Sombrio", "Reborned", listIngresso2);
-        evento2.criaLocalData(1995, 1, 1, "Arena Ceres");
-        evento2.criaLocalData(2020, 9, 21, "Alexandria");
-        evento2.criaLocalData(2006, 6, 6, "Broadway");
+    Evento evento2 = new Evento("Let e Thales - Um Encontro Sombrio", "Reborned", listIngresso2);
+    evento2.criaLocalData(1995, 1, 1, "Arena Ceres");
+    evento2.criaLocalData(2020, 9, 21, "Alexandria");
+    evento2.criaLocalData(2006, 6, 6, "Broadway");
 
-        // Criamos uma lista de eventos para conseguirmos iterar sobre eles e apresentar
-        List<Evento> eventos = new ArrayList<>();
-        eventos.add(evento1);
-        eventos.add(evento2);
+    // Criamos uma lista de eventos para conseguirmos iterar sobre eles e apresentar
+    List<Evento> eventos = new ArrayList<>();
+    eventos.add(evento1);
+    eventos.add(evento2);
 
-        // Declaramos nossa variável scanner para receber dados de entrada do usuário
-        Scanner teclado = new Scanner(System.in);
+    // Declaramos nossa variável scanner para receber dados de entrada do usuário
+    Scanner teclado = new Scanner(System.in);
 
-        // Começamos a iterar e apresentar os dados na tela
-        int opcao;
-        do {
-            // Interface amigável
-            System.out.println("Oi, chuchu! Bem vindo à Lojinha dos Bilhetes Intergaláticos :) \n");
-            System.out.println("Digite sua opção: ");
-            System.out.println("1 - Comprar um Ingresso");
-            System.out.println("0 - Sair\n");
+    // Começamos a iterar e apresentar os dados na tela
+    int opcao;
+    do {
+      // Interface amigável
+      System.out.println("Oi, chuchu! Bem vindo à Lojinha dos Bilhetes Intergaláticos :) \n");
+      System.out.println("Digite sua opção: ");
+      System.out.println("1 - Comprar um Ingresso");
+      System.out.println("0 - Sair\n");
 
-            // Aqui é onde vai começar a interaçao com o user
-            opcao = teclado.nextInt();
-            if (opcao == 1){
-                System.out.println("Escolha um dos Eventos:");
-                for (int i = 0 ; i < eventos.size() ; i++){
-                    System.out.println("Evento " + (i + 1) + ": " + eventos.get(i).getInformacoes());
-                }
-                opcao = teclado.nextInt();
-                if ((opcao - 1) > eventos.size()){
-                    System.out.println("Evento não existe");
-                }
-                else {
-                    // aqui ele já escolheu o show e vai escolher a data
-										String chosenEvento = eventos.get(opcao-1).getInformacoes();
-										int numEvento = opcao-1;
-										E = eventos.get(numEvento);
-                    System.out.println("Você escolheu: " + chosenEvento + " Por favor escolha um local e uma data:\n");
-                    // alguém coloca aqui numeros nos locais plz
-                    E.getLocais();
+      // Aqui é onde vai começar a interaçao com o user
+      opcao = teclado.nextInt();
+      if (opcao == 1){
+        System.out.println("Escolha um dos Eventos:");
+        for (int i=0; i<eventos.size(); i++){
+	        System.out.println("Evento " + (i + 1) + ": " + eventos.get(i).getInformacoes());
+        }
+        opcao = teclado.nextInt();
+        if ((opcao-1) > eventos.size()){
+	        System.out.println("Evento não existe");
+        }
+        else {
+          // aqui ele já escolheu o show e vai escolher a data
+					String chosenEvento = eventos.get(opcao-1).getInformacoes();
+					int numEvento = opcao-1;
+					E = eventos.get(numEvento);
+					System.out.println("Você escolheu: " + chosenEvento + " Por favor escolha um local e uma data:\n");
+					E.getLocais();
+					
+					// aqui ele tem show, local e data e vai escolher um ingresso
+          opcao = teclado.nextInt();
+          String chosenLocal = E.getLocalEscolhido(opcao-1);
+          int numLocal = opcao-1;
+          System.out.println("Você escolheu: " + chosenEvento + " Em: " + chosenLocal + ". Escolha seu tipo de ingresso: ");
+          E.getIngressosAtuais();
+                  
+          // aqui ele tem show, local e data e escolhe tipo ingresso + quantidade
+					opcao = teclado.nextInt();
+					int numIngresso = opcao-1;
+					System.out.println("Você escolheu o ingresso: " + 
+					E.getIngressos().get(numIngresso).getTipoIngressonoBilhete() + 
+					" que custa: " + E.getIngressos().get(numIngresso).getValorIngressoNoBilhete() + ".");
+          System.out.println("Digite a quantidade: ");
+					int quantidade = teclado.nextInt();
+                
+          // aqui ele confirma a compra
+          System.out.println("Deseja continuar a sua compra? Digite '1' para continuar ou '0' para cancelar.");
+          int confirma = teclado.nextInt();
+          if (confirma == 0) {
+						break;
+					}
 
-                    // aqui ele tem show, local e data e vai escolher um ingresso
-                    opcao = teclado.nextInt();
-                    String chosenLocal = E.getLocalEscolhido(opcao-1);
-                    int numLocal = opcao-1;
-                    System.out.println("Você escolheu: " + chosenEvento + " Em: " + chosenLocal + ". Escolha seu tipo de ingresso: ");
-                    //mostra os tipos de ingressos escolhidos
-                    E.getIngressosAtuais();
+					//Relaciona quantidade de ingressos e o ingresso escolhido na classe QtdCarrinho
+          QtdCarrinho qtd = new QtdCarrinho(E.getIngressos().get(opcao-1), quantidade);
+          Cart.adicionaNoCarrinho(qtd);
+          Cart.mostraCarrinho();
+          System.out.println("Você tem certeza que quer continuar? Digite '1' para continuar ou '0' para cancelar.");
+          confirma = teclado.nextInt();
+          if (confirma == 0){
+            break;
+          }
 
-                    
-                    // aqui ele tem show, local e data e escolhe tipo ingresso + quantidade
-										opcao = teclado.nextInt();
-										int numIngresso = opcao-1;
-										System.out.println("Você escolheu o ingresso: " + 
-										E.getIngressos().get(numIngresso).getTipoIngressonoBilhete() + 
-										" que custa: " + E.getIngressos().get(numIngresso).getValorIngressoNoBilhete() + ".");
-                    System.out.println("Digite a quantidade: ");
-										int quantidade = teclado.nextInt();
-                    
-                    // aqui ele confirma a compra
-                    System.out.println("Deseja continuar a sua compra? Digite '1' para continuar ou '0' para cancelar.");
-                    int confirma = teclado.nextInt();
-                    if (confirma == 0) {
-												break;
-										}
-
-                    //Relaciona quantidade de ingressos e o ingresso escolhido na classe QtdCarrinho
-                    QtdCarrinho qtd = new QtdCarrinho(E.getIngressos().get(opcao-1), quantidade);
-                    Cart.adicionaNoCarrinho(qtd);
-                    Cart.mostraCarrinho();
-                    System.out.println("Você tem certeza que quer continuar? Digite '1' para continuar ou '0' para cancelar.");
-                    confirma = teclado.nextInt();
-                    if (confirma == 0){
-                        break;
-                    }
-
-                    System.out.println("Oba! Vamos continuar. Digite o número do seu cartão:");
-                    String numeroDoCartao = teclado.nextLine();
-                    
-                }
-            }
-        } while (opcao != 0); // enquanto a primeira resposta não for 0 o sistema roda
-        
-    }
-}
+          System.out.println("Oba! Vamos continuar. Digite o número do seu cartão:");
+          String numeroDoCartao = teclado.nextLine();             
+       	 }
+     	 }
+			} 
+			while (opcao != 0); // enquanto a primeira resposta não for 0 o sistema roda
+  	}
+	}
     // lembrar de fechar o teclado!!!!!!!!!!!!!!!!!
